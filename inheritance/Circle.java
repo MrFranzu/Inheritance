4869579c9c032dad60a774fc41bb25c18cdfbeed
Amiel Franz Bagui <118027434+MrFranzu@users.noreply.github.com>
2024-07-18 12:34:08 +0800

    Add files via upload

---
+++ inheritance/Circle.java
@@ -0,0 +1,12 @@
+public class Circle extends Shape {
+    private double radius;
+
+    public Circle(String color, String texture, double radius) {
+        super(color, texture);
+        this.radius = radius;
+    }
+
+    public double getArea() {
+        return 3.14 * radius * radius;
+    }
+}
